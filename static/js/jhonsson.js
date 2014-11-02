38a0c0d31a2dce1196d9e3e415cd35d267019020
Andrea Anabell Loaiza Gonzaga <andreitaloaiza0511@gmail.com>
2014-11-02 08:16:08 -0500

    02/11/14 - Implementado Reloj JS

--- static/js/jhonsson.js
+++ static/js/jhonsson.js
@@ -18,3 +18,33 @@ function envioRecepcionAjax(url, params)
         castear(content);
     });
 }
+
+function hora()
+{
+    var  dia = new Date();
+    var  hora = dia.getHours();
+    var  minutos = dia.getMinutes();
+    var  segundos = dia.getSeconds();
+
+    var meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+    var diasSemana = ["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"];
+
+    if ((hora >= 0)&&(hora <= 9)){
+        hora="0"+hora;
+    }
+
+    if ((minutos >= 0)&&(minutos <= 9)){
+        minutos="0"+minutos;
+    }
+
+    if ((segundos >= 0)&&(segundos <= 9)){
+        segundos="0"+segundos;
+    }
+
+    document.getElementById("hora").
+        innerHTML =  " "  + hora + ":" + minutos + ":" + segundos;
+    document.getElementById("fecha").
+        innerHTML =  " "  + diasSemana[dia.getDay()] + ", " + dia.getDate() + ". " + meses[dia.getMonth()]+" "+dia.getFullYear();
+
+    window.setTimeout("hora()",1000);
+}
